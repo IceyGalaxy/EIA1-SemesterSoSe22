4ee371bec5df5d28073dfc70cf5ad48ee1b02ee6
IceyGalaxy <ioanna.gkaroumpas@hs-furtwangen.de>
2022-05-07 18:55:21 +0200

    aufg5

---
+++ Aufgabe_6/script.js
@@ -0,0 +1,17 @@
+var DE2008 = 82000000;
+var DE2022 = 83200000;
+let resDE04 = (DE2022 - DE2008) / DE2008 * 100.0;
+console.log("Wachstumsrate seit 2008 in Deutschland" + resDE04 + "%");
+var FRA2008 = 62140000;
+var FRA2022 = 65480000;
+let resFRA04 = (FRA2022 - FRA2008) / FRA2008 * 100.0;
+console.log("Wachstumsrate seit 2008 in Frankreich" + resFRA04 + "%");
+var ITA2008 = 59000000;
+var ITA2022 = 59580000;
+let resITA04 = (ITA2022 - ITA2008) / ITA2008 * 100.0;
+console.log("Wachstumsrate seit 2008 in Italien" + resITA04 + "%");
+var ESP2008 = 45980000;
+var ESP2022 = 47360000;
+let resESP04 = (ESP2022 - ESP2008) / ESP2008 * 100.0;
+console.log("Wachstumsrate seit 2008 in Spanien" + resESP04 + "%");
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe_6/script.ts
@@ -0,0 +1,27 @@
+var DE2008: number=82000000;
+var DE2022: number=83200000;
+
+let resDE04=(DE2022-DE2008) / DE2008 * 100.0;
+
+console.log("Wachstumsrate seit 2008 in Deutschland" + resDE04 + "%" );
+
+var FRA2008: number=62140000;
+var FRA2022: number=65480000;
+
+let resFRA04= (FRA2022-FRA2008) / FRA2008 * 100.0;
+
+console.log("Wachstumsrate seit 2008 in Frankreich" + resFRA04 + "%" );
+
+var ITA2008: number=59000000 ;
+var ITA2022: number=59580000 ;
+
+let resITA04=(ITA2022-ITA2008) / ITA2008 * 100.0;
+
+console.log("Wachstumsrate seit 2008 in Italien" + resITA04 + "%" );
+
+var ESP2008: number=45980000;
+var ESP2022: number=47360000;
+
+let resESP04=(ESP2022-ESP2008) / ESP2008 * 100.0;
+
+console.log("Wachstumsrate seit 2008 in Spanien" + resESP04 + "%" );
