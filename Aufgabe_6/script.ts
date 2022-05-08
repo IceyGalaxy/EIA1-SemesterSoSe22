41e067f36ca3fcc5b547a9d92d2e51d846cccbf4
IceyGalaxy <ioanna.gkaroumpas@hs-furtwangen.de>
2022-05-08 17:19:23 +0200

    aufg6

--- Aufgabe_6/script.ts
+++ Aufgabe_6/script.ts
@@ -3,25 +3,25 @@ var DE2022: number=83200000;
 
 let resDE04=(DE2022-DE2008) / DE2008 * 100.0;
 
-console.log("Wachstumsrate seit 2008 in Deutschland" + resDE04 + "%" );
+console.log("Die Wachstumsrate seit 2008 in Deutschland beträgt " + resDE04 + "%" );
 
 var FRA2008: number=62140000;
 var FRA2022: number=65480000;
 
 let resFRA04= (FRA2022-FRA2008) / FRA2008 * 100.0;
 
-console.log("Wachstumsrate seit 2008 in Frankreich" + resFRA04 + "%" );
+console.log("Die Wachstumsrate seit 2008 in Frankreich beträgt " + resFRA04 + "%" );
 
 var ITA2008: number=59000000 ;
 var ITA2022: number=59580000 ;
 
 let resITA04=(ITA2022-ITA2008) / ITA2008 * 100.0;
 
-console.log("Wachstumsrate seit 2008 in Italien" + resITA04 + "%" );
+console.log("Die Wachstumsrate seit 2008 in Italien beträgt " + resITA04 + "%" );
 
 var ESP2008: number=45980000;
 var ESP2022: number=47360000;
 
 let resESP04=(ESP2022-ESP2008) / ESP2008 * 100.0;
 
-console.log("Wachstumsrate seit 2008 in Spanien" + resESP04 + "%" );
+console.log("Die Wachstumsrate seit 2008 in Spanien beträgt " + resESP04 + "%" );
