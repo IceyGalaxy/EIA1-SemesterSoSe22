468357208b70c6d75663e3f6537b10d724fec496
IceyGalaxy <ioanna.gkaroumpas@hs-furtwangen.de>
2022-05-24 17:40:18 +0200

    aufg8

--- Aufgabe_8/drumpad.ts
+++ Aufgabe_8/drumpad.ts
@@ -16,16 +16,16 @@ setInterval(function() {
 
  }, 500);
  
-let sounds = [];
-sounds[0] = new Audio("L08_task_material_assets_DrumPad_A.mp3");
-sounds[1] = new Audio("L08_task_material_assets_DrumPad_C.mp3");
-sounds[2] = new Audio("L08_task_material_assets_DrumPad_F.mp3");
-sounds[3] = new Audio("L08_task_material_assets_DrumPad_G.mp3");
-sounds[4] = new Audio("L08_task_material_assets_DrumPad_hihat.mp3");
-sounds[5] = new Audio("L08_task_material_assets_DrumPad_kick.mp3");
-sounds[6] = new Audio("L08_task_material_assets_DrumPad_laugh-1.mp3");
-sounds[7] = new Audio("L08_task_material_assets_DrumPad_laugh-2.mp3");
-sounds[8] = new Audio("L08_task_material_assets_DrumPad_snare.mp3");
+let sounds:HTMLAudioElement[] = [];
+sounds[0] = new Audio('assets/Drumpad_A.mp3');
+sounds[1] = new Audio('assets/DrumPad_C.mp3');
+sounds[2] = new Audio('assets/DrumPad_F.mp3');
+sounds[3] = new Audio('assets/DrumPad_G.mp3');
+sounds[4] = new Audio('assets/DrumPad_hihat.mp3');
+sounds[5] = new Audio('assets/DrumPad_kick.mp3');
+sounds[6] = new Audio('assets/DrumPad_laugh1.mp3');
+sounds[7] = new Audio('assets/DrumPad_laugh2.mp3');
+sounds[8] = new Audio('assets/DrumPad_snare.mp3');
 
 let beatfolge = [];
 beatfolge[0] = sounds[5];
@@ -38,4 +38,12 @@ function playSample(sound:HTMLAudioElement){
 }
 window.addEventListener("load", function(){
 document.querySelector(".drum1").addEventListener("click", function () { playSample(sounds[0]) });
+document.querySelector(".drum2").addEventListener("click", function () { playSample(sounds[1]) });
+document.querySelector(".drum3").addEventListener("click", function () { playSample(sounds[2]) });
+document.querySelector(".drum4").addEventListener("click", function () { playSample(sounds[3]) });
+document.querySelector(".drum5").addEventListener("click", function () { playSample(sounds[4]) });
+document.querySelector(".drum6").addEventListener("click", function () { playSample(sounds[5]) });
+document.querySelector(".drum7").addEventListener("click", function () { playSample(sounds[6]) });
+document.querySelector(".drum8").addEventListener("click", function () { playSample(sounds[7]) });
+document.querySelector(".drum9").addEventListener("click", function () { playSample(sounds[8]) });
 })
